e2c9edbd430f21322cc12ffb73a42c902f4947fe
Carlos David Páez Ferreira <carlos.paezf@usantoto.edu.co>
2022-03-21 16:10:05 -0500

    Protected - Sidenav

--- frontend-leis/src/app/protected/protected.module.ts
+++ frontend-leis/src/app/protected/protected.module.ts
@@ -1,17 +1,22 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+
 import { ProtectedRoutingModule } from './protected-routing.module';
 import { MainComponent } from './pages/main/main.component';
+import { SidenavComponent } from './shared/sidenav/sidenav.component';
+import { PrimeNgModule } from '../prime-ng/prime-ng.module';
 
 
 
 @NgModule({
 	declarations: [
-		MainComponent
+		MainComponent,
+		SidenavComponent
 	],
 	imports: [
 		CommonModule,
-		ProtectedRoutingModule
+		ProtectedRoutingModule,
+		PrimeNgModule
 	]
 })
 export class ProtectedModule { }
