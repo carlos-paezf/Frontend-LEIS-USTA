64777b0211074f87a02477d1e9169a40f5e8b4ff
Sergio Mesa <davidmesab7@gmail.com>
2022-03-30 17:13:22 -0500

    protected-loan-equipment-loan

--- frontend-leis/src/app/protected/modules/loan/loan.module.ts
+++ frontend-leis/src/app/protected/modules/loan/loan.module.ts
@@ -3,11 +3,13 @@ import { CommonModule } from '@angular/common';
 
 import { LoanRoutingModule } from './loan-routing.module';
 import { ListComponent } from './pages/list/list.component';
+import { EquipmentLoanComponent } from './pages/equipment-loan/equipment-loan.component';
 
 
 @NgModule({
     declarations: [
-        ListComponent
+        ListComponent,
+        EquipmentLoanComponent
     ],
     imports: [
         CommonModule,
