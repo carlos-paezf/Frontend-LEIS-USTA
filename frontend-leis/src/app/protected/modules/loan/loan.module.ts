d0a544dba3622c80e1e1731d8b0720cf637ee6e7
Sergio Mesa <davidmesab7@gmail.com>
2022-03-30 20:35:58 -0500

    Se crearon lo componentes de loan-request y return-loan

--- frontend-leis/src/app/protected/modules/loan/loan.module.ts
+++ frontend-leis/src/app/protected/modules/loan/loan.module.ts
@@ -4,12 +4,16 @@ import { CommonModule } from '@angular/common';
 import { LoanRoutingModule } from './loan-routing.module';
 import { ListComponent } from './pages/list/list.component';
 import { EquipmentLoanComponent } from './pages/equipment-loan/equipment-loan.component';
+import { LoanRequestComponent } from './pages/loan-request/loan-request.component';
+import { ReturnLoanComponent } from './pages/return-loan/return-loan.component';
 
 
 @NgModule({
     declarations: [
         ListComponent,
-        EquipmentLoanComponent
+        EquipmentLoanComponent,
+        LoanRequestComponent,
+        ReturnLoanComponent
     ],
     imports: [
         CommonModule,
---
+++ frontend-leis/src/app/protected/modules/loan/pages/return-loan/return-loan.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-return-loan',
+  templateUrl: './return-loan.component.html',
+  styleUrls: ['./return-loan.component.css']
+})
+export class ReturnLoanComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
