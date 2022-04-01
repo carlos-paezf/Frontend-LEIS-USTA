56dbca2b6a227237980cf78adb0193fa4fc0f94d
JairoGarcia16 <jairoeduardo14@gmail.com>
2022-03-31 21:07:45 -0500

    Paginas Listar, Fichas y manejo de equipos

--- frontend-leis/src/app/protected/modules/equipment/equipment.module.ts
+++ frontend-leis/src/app/protected/modules/equipment/equipment.module.ts
@@ -4,16 +4,23 @@ import { CommonModule } from '@angular/common';
 import { EquipmentRoutingModule } from './equipment-routing.module';
 import { ListComponent } from './pages/list/list.component';
 import { TechnicalDataSheetsComponent } from './pages/technical-data-sheets/technical-data-sheets.component';
-
+import { MaterialDesignModule } from 'src/app/material-design/material-design.module';
+import { ManageUsersComponent } from './pages/manage-users/manage-users.component';
+import { PrimeNgModule } from 'src/app/prime-ng/prime-ng.module';
 
 @NgModule({
     declarations: [
         ListComponent,
-        TechnicalDataSheetsComponent
+        TechnicalDataSheetsComponent,
+        ManageUsersComponent,
+
     ],
     imports: [
         CommonModule,
-        EquipmentRoutingModule
+        EquipmentRoutingModule,
+        MaterialDesignModule,
+        PrimeNgModule
+
     ]
 })
 export class EquipmentModule { }
