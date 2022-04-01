01a0b3c148e7ece0a7a627531edc96843dbc023d
JairoGarcia16 <jairoeduardo14@gmail.com>
2022-03-31 21:04:24 -0500

    Implementación de imports primeng

--- frontend-leis/src/app/prime-ng/prime-ng.module.ts
+++ frontend-leis/src/app/prime-ng/prime-ng.module.ts
@@ -1,12 +1,22 @@
 import { NgModule } from '@angular/core';
 import { BreadcrumbModule } from 'primeng/breadcrumb'
 import { BadgeModule } from 'primeng/badge';
-
-
+import {FileUploadModule} from 'primeng/fileupload';
+import {HttpClientModule} from '@angular/common/http';
+import { ToastrModule } from 'ngx-toastr';
+import { TableModule } from 'primeng/table';
+import {DropdownModule} from 'primeng/dropdown';
+import {MultiSelectModule} from 'primeng/multiselect';
 @NgModule({
     exports: [
         BreadcrumbModule,
         BadgeModule,
+        FileUploadModule,
+        HttpClientModule,
+        ToastrModule,
+        TableModule,
+        DropdownModule,
+        MultiSelectModule
     ]
 })
 export class PrimeNgModule { }
