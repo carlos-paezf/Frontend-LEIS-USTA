c200c4b7703d2d6eb7e1e03fd5d861e734ec2c8b
Sergio Alejandro <sergio.gile@usantoto.edu.co>
2022-03-31 22:55:40 -0500

    imports Material

--- frontend-leis/src/app/material-design/material-design.module.ts
+++ frontend-leis/src/app/material-design/material-design.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
-import { MatAutocompleteModule } from '@angular/material/autocomplete'
-
-
+import { MatAutocompleteModule } from '@angular/material/autocomplete';
+import { FormsModule } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
     exports: [
-        MatAutocompleteModule
+      MatAutocompleteModule,
+      FormsModule,
+      ReactiveFormsModule
     ]
 })
 export class MaterialDesignModule { }
